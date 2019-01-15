e724ac850469e8f80137bd00695adde03e33bcf3
bruno.cesar-work <bruno.cesar@makrosystems.com.br>
2019-01-15 17:55:47 -0200

    Configuracao das cores de layout e cricacao de pacotes

--- app/src/main/java/br/com/uber/brunoclas/uber/activity/MapsActivity.java
+++ app/src/main/java/br/com/uber/brunoclas/uber/activity/MapsActivity.java
@@ -1,4 +1,4 @@
-package br.com.uber.brunoclas.uber;
+package br.com.uber.brunoclas.uber.activity;
 
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
@@ -9,6 +9,8 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import br.com.uber.brunoclas.uber.R;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
